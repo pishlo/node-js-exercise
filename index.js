1af799e8609a7b65ada9409f5ed39090af36c4ba
pishlo <soni.kozaliev@gmail.com>
2024-10-07 14:55:27 +0200

    finished the exercise

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("test")
+const userOne = {
+    name: "Marie",
+    age: 20,
+    courses: []
+}
+userOne.age = 21
+userOne.grade = "A"
+
+userOne.courses.push("Math")
+userOne.courses.push("Physics")
+userOne.courses.push("Chemistry")
+
+const physicsCourse = userOne.courses.indexOf("Physics")
+const newArray = userOne.courses.slice(0, 2)
+userOne.array = newArray
+
+console.log(userOne)
+console.log(physicsCourse)
